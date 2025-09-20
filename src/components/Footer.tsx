3415f623b0bf4504b17320f0690e43191c324c5f
vinh011112 <vinh011112@gmail.com>
2025-09-21 01:30:21 +0900

    Create Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+export const Footer: React.FC = () => {
+  return (
+    <footer className="w-full py-4 mt-8">
+      <div className="container mx-auto text-center text-gray-400 text-sm">
+        <p>Copyright 2025 @ Developing by VINH</p>
+      </div>
+    </footer>
+  );
+};
